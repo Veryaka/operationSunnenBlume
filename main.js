2e1aba8ba449db6061d0d485134adf2e851f45f1
Veryaka <arifinnurpadillah@gmail.com>
2023-11-11 10:07:23 +0700

    first commit

--- main.js
+++ main.js
@@ -99,3 +99,64 @@ function newElement() {
     };
   }
 }
+
+var slides = document.querySelectorAll(".slide");
+var buttons = document.querySelectorAll(".slider-btn");
+let currentSlide = 1;
+
+var manualNav = function (manual) {
+  slides.forEach(function (slide) {
+    slide.classList.remove("active");
+
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
+
+  slides[manual].classList.add("active");
+  buttons[manual].classList.add("active");
+};
+
+buttons.forEach(function (btn, i) {
+  btn.addEventListener("click", function () {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
+
+  let i = 1;
+  var repeater = function () {
+    setTimeout(function () {
+      [...active].forEach(function (activeSlide) {
+        activeSlide.classList.remove("active");
+      });
+      slides[i].classList.add("active");
+      buttons[i].classList.add("active");
+      i++;
+
+      if (slides.length == 1) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 5000);
+  };
+  repeater();
+};
+
+repeat();
+
+var btnSubmit = document.querySelector(".submit");
+var modal = document.querySelector(".modal-container");
+if (btnSubmit) {
+  btnSubmit.addEventListener("click", function () {
+    modal.classList.add("show");
+    const next = document.querySelector(".next");
+    next.setAttribute("value", "https://operationsonnenblume.netlify.app");
+  });
+}
